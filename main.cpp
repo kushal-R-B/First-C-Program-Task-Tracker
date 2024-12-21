f5aca9b2d5e01ce36aef07443bfd72b1b61d59fb
Kushal <kushal.R.B@outlook.com>
2024-12-21 10:26:55 -0600

    Vers2.0

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ class Task{
 
 //  @brief Prints the tasks from the inputted vector.
 //  @param taskList a vector that contains either tasks objects to complete or tasks objects that have yet to be completed.
-void printTasks(std::vector<Task> taskList){
+void printTasks(std::vector<Task>& taskList){
     cout << "\n--- Task List ---\n";
     for(int i = 0 ; i < taskList.size() ; i++){
                 cout << i << ". " <<taskList[i].getTaskName() << endl;
@@ -45,6 +45,15 @@ void printTasks(std::vector<Task> taskList){
     cout << "-----------------\n";
 }
 
+// @brief Takes a task list and number in the vector and prints out a divider containg the information.
+// @param taskList a vector that contains the task needed to be printed.
+// @param an integer that tells which task, based off position in the vector, that needs to be printed out.
+void printDescription(std::vector<Task>& taskList, String userInputedInteger){
+    cout << "-----------------\n" << std::flush;
+    cout << taskList[std::stoi(userInputedInteger)].getTaskName() << ": " << endl;
+    cout << taskList[std::stoi(userInputedInteger)].getTaskDescription() << endl;
+    cout << "-----------------\n" << std::flush;
+}
 
 
 int main(){
@@ -59,38 +68,44 @@ int main(){
         //First menu: options 1 or 2, and option 3 to add tasks to the list.
         cout << "Menus:" << endl;
         cout << "Option 1: Tasks to Do" << endl;
-        cout << "Option 2: Tasks to Completed" << endl;
+        cout << "Option 2: Tasks Completed" << endl;
         cout << "Option 3: Add Tasks" << endl;
         cout <<"Enter user input: " <<std::flush;
         
         std::getline(cin,userInput);
         //Option 1 is to view tasks that have not been completed. 
         //Suboption menus to look at task description and then have an option to count as complete.
-        if(userInput == "1"){
-            printTasks(toDoTasks);
-            cout << "Which tasks would you like to view?: " << endl;
-            cin >> userInput;
-
-            cout << "-----------------\n" << std::flush;
-            cout << toDoTasks[std::stoi(userInput)].getTaskName() << ": " << endl;
-            cout << toDoTasks[std::stoi(userInput)].getTaskDescription() << endl;
-            cout << "-----------------\n" << std::flush;
-            
-            do
-            {
-                cout << "Would you like to view another task?(Y/N): ";
-                cin >> userInput;
-                if(userInput == "Y"){
+        if (userInput == "1") {
+    // Prints list of tasks and asks if the viewer would like to view in more detail.
+            do {
+                if (static_cast<int>(toDoTasks.size()) != 0) {
                     printTasks(toDoTasks);
-                    cout << "Which tasks would you like to view?: " << endl;
+                    cout << "Which task would you like to view?: " << endl;
                     cin >> userInput;
-                    cout << "-----------------\n" << std::flush;
-                    cout << toDoTasks[std::stoi(userInput)].getTaskDescription() << endl;
-                    cout << "-----------------\n" << std::flush;
-                }
-                else{
+
+                    // Temporary storage of input.
+                    int tempInt = std::stoi(userInput);
+                    printDescription(toDoTasks, userInput);
+
+                    cout << "Would you like to mark this task as complete? (Y/N): ";
+                    cin >> userInput;
+
+                    if (userInput == "Y") {
+                        // Store the task to be removed as a reference.
+                        Task tempTask = toDoTasks[tempInt];
+                        // Remove the task.
+                        toDoTasks.erase(toDoTasks.begin() + tempInt);
+                        // Add the task to the completed task list.
+                        completedTasks.push_back(tempTask);
+                    }
+                } else {
+                    cout << "No tasks to view... Add more tasks!" << endl <<std::flush;
+                    userInput = "N";
                     break;
                 }
+
+                cout << "Would you like to view another task? (Y/N): ";
+                cin >> userInput;
 
             } while (userInput != "N");
         }
@@ -106,7 +121,7 @@ int main(){
         if(userInput == "3"){
             do
             {
-                //Task Name, // @bug the getline in this is not getting this line.
+                //Task Name, 
                 cout << "Task Name: " << std::flush; 
                 std::getline(cin, userInput); 
                 String tempName = userInput; 
@@ -133,6 +148,7 @@ int main(){
                 
             }while (userInput != "N");
         }
+        //Refresh the input stream.
         std::cin.ignore();
     }
     return 0; 
